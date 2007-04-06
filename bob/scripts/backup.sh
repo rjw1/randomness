a26ceb70a3c903ab3d40139e0fa03dc69e8839d8
bob walker <bob@randomness.org.uk>
2007-04-06 22:27:53 +0000

    backup more databases.

--- bob/scripts/backup.sh
+++ bob/scripts/backup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
-/usr/bin/pg_dump -Z 9 -f /export/home/bob/backups/london.sql.`/usr/bin/date +%Y%m%d`.gz -d london
+for db in london capetown
+do
+/usr/bin/pg_dump -Z 9 -f /export/home/bob/backups/$db.sql.`/usr/bin/date +%Y%m%d`.gz -d $db
+done
 /opt/csw/bin/svnadmin dump -q /export/home/repository/ | bzip2 > /export/home/bob/backups/svndump.`/usr/bin/date +%Y%m%d`.bz2
 /opt/csw/bin/rsync -aqz /export/home/bob/ theproject.fierypit.org:nebulabackup/

7958db8b41bed77c658dd96c43c091776f1dd331
Kake Pugh <kake@earth.li>
2007-04-21 02:23:59 +0000

    Add thingy to RGL::Addons to make dropdown box of all Tube stations.

--- london.randomness.org.uk/scripts/lib/RGL/Addons.pm
+++ london.randomness.org.uk/scripts/lib/RGL/Addons.pm
@@ -24,4 +24,39 @@ sub get_tt_vars {
   return %tt_vars;
 }
 
+sub get_tube_dropdown {
+  my ( $self, %args ) = @_;
+
+  my $q = $args{q};
+  my $guide = $args{guide};
+  my $dbh = $guide->wiki->store->dbh;
+
+  my $sql = "SELECT node.name FROM node
+INNER JOIN metadata as tube
+  ON ( node.id = tube.node_id AND node.version = tube.version
+       AND lower( tube.metadata_type ) = 'category'
+       AND lower( tube.metadata_value ) = 'tube'
+       AND node.name NOT LIKE 'Category%'
+     )
+  ORDER BY node.name
+";
+
+  my $sth = $dbh->prepare( $sql );
+  $sth->execute or die $dbh->errstr;
+
+  my @tubes;
+  while ( my ( $tube ) = $sth->fetchrow_array ) {
+    push @tubes, $tube;
+  }
+
+  my $any_string = " -- any -- ";
+  my $box = $q->popup_menu( -name   => "tube",
+                            -values => [ "", @tubes ],
+                            -labels => { "" => $any_string,
+                                         map { $_ => $_ } @tubes },
+                           );
+
+  return $box;
+}
+
 1;

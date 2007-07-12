104c4b10c4321eedfffb6052b219a7259134230d
bob walker <bob@randomness.org.uk>
2007-07-12 08:51:53 +0000

    add backup to rsync.net. also add more databases to back up.

--- bob/scripts/backup.sh
+++ bob/scripts/backup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
-for db in london capetown vienna
+for db in london capetown vienna bobwiki circlewiki wpewiki
 do
 /usr/bin/pg_dump -Z 9 -f /export/home/bob/backups/$db.sql.`/usr/bin/date +%Y%m%d`.gz -d $db
 done
 /opt/csw/bin/svnadmin dump -q /export/home/repository/ | bzip2 > /export/home/bob/backups/svndump.`/usr/bin/date +%Y%m%d`.bz2
 /opt/csw/bin/rsync -aqz /export/home/bob/ theproject.fierypit.org:nebulabackup/
+/opt/csw/bin/rsync -aqz /export/home/bob/ ch-s010.rsync.net:nebula/
 cp /export/home/bob/backups/london.sql.`/usr/bin/date +%Y%m%d`.gz /export/home/bob/web/vhosts/london.randomness.org.uk/dbdump/rgl.sql.gz
 cp /export/home/bob/backups/vienna.sql.`/usr/bin/date +%Y%m%d`.gz /export/home/bob/web/vhosts/vienna.openguides.org/dbdump/vienna.sql.gz

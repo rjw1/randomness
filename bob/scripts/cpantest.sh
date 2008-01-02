5e3acb3c068cb1713e66a7898fcd723c437bae03
bob walker <bob@randomness.org.uk>
2008-01-02 21:38:07 +0000

    do perl-5.10.0 as well.

--- bob/scripts/cpantest.sh
+++ bob/scripts/cpantest.sh
@@ -4,3 +4,6 @@ echo $MODULE
 rm -rf /export/home/bob/cpantesting/perl-5.8.8
 gtar -zxf /export/home/bob/cpantesting/perl-5.8.8.tar.gz -C /export/home/bob/cpantesting/
 /export/home/bob/cpantesting/perl-5.8.8/bin/cpan $MODULE
+rm -rf /export/home/bob/cpantesting/perl-5.10.0
+gtar -zxf /export/home/bob/cpantesting/perl-5.10.0.tar.gz -C /export/home/bob/cpantesting/
+/export/home/bob/cpantesting/perl-5.10.0/bin/cpan $MODULE

4ed266392322e5739149541233153e5ad749ddcc
Kake Pugh <kake@earth.li>
2008-07-22 22:52:38 +0000

    More heuristics.

--- london.randomness.org.uk/scripts/lib/RGL/SpamFinder.pm
+++ london.randomness.org.uk/scripts/lib/RGL/SpamFinder.pm
@@ -18,6 +18,7 @@ sub looks_like_spam {
     my $content = $args{content};
     my $username = $args{metadata}{username};
     my $host = $args{metadata}{host};
+    my $comment = $args{added_comment} || "";
 
     if ( $content =~ /\b(viagra|cialis|supermeganah|tramadol|vicodin|phentermine|buyphentermine|adipex|phendimetrazine|ephedrine|lipitor|hydrocodone|replica-watches)\b/is ) {
         $class->notify_admins( %args, reason => "Matches $1" );
@@ -68,12 +69,23 @@ sub looks_like_spam {
             return 1;
         }
 
-        if ( $args{added_comment} =~ m/(bibi-nibe|akir-nime|niva-tope|mila-yela|lopi-niza|madu-lika|aiva-nima|hite-buri|rews-kimd|kile-bibi|terveron|rexi-vild|reza-blat|lize-vida|dive-luni|lize111|bestgreatworld\.info|greatworldbank\.info|kelia.freehostia.com|jinerbond|nudestar.uni.cc|sexformnude.uni.cc|kamasutranet.co.cc|lipchild|finentikal|pendosegi)/i ) {
+        if ( $username =~ m/(m0wz0a6e8ds|m0wzdgs)/i ) {
+            $class->notify_admins( %args, reason => "user: $1" );
+            return 1;
+        }
+
+        if ( $comment =~ m/(\+.*){8,}/ ) {
+            $class->notify_admins( %args,
+                        reason => "comment with more than 8 plus signs in" );
+            return 1;
+        }
+
+        if ( $comment =~ m/(bibi-nibe|akir-nime|niva-tope|mila-yela|lopi-niza|madu-lika|aiva-nima|hite-buri|rews-kimd|kile-bibi|terveron|rexi-vild|reza-blat|lize-vida|dive-luni|lize111|bestgreatworld\.info|greatworldbank\.info|kelia.freehostia.com|jinerbond|nudestar.uni.cc|sexformnude.uni.cc|kamasutranet.co.cc|lipchild|finentikal|pendosegi|staggytheboyscoutsla|fjrf.3vindia.info)/i ) {
             $class->notify_admins( %args, reason => "$1" );
             return 1;
         }
 
-        if ( $name eq "Websites About London" && $args{added_comment} =~ m/(www.google.us|us.cyworld.com)/ ) {
+        if ( $name eq "Websites About London" && $comment =~ m/(www.google.us|us.cyworld.com|www.imeem.com|freeiq.com)/ ) {
             $class->notify_admins( %args, reason => "Websites About London, $1" );
             return 1;
         }
@@ -83,12 +95,18 @@ sub looks_like_spam {
             return 1;
         }
 
-        if ( $name eq "Websites About London" && $args{added_comment} =~ m!groups.google.us! ) {
+        if ( $name eq "Websites About London" && $comment =~ m!groups.google.us! ) {
             $class->notify_admins( %args, reason => "groups.google.us/WAL" );
             return 1;
         }
 
-        if ( ( $name eq "Nicolas, SW6 4ST" || $name eq "Old Pack Horse, W4 5TF" || $name eq "Oriental Brasserie, W4 2HD" ) && $args{added_comment} =~ m!http://[-.a-z0-9]+\.(co.cc|uni.cc|talk4fun.net|297m.com|1vn.biz|aokhost.com|freeweb7.com|myokhost.com|22web.net|totalh.com|10001mb.com|isgreat.org|66ghz.com|iblogger.org|byethost12.com|20xhost.com|yourhelpful.net|fasthoster.info|happyhost.org|by.ru)(\s|/)! ) {
+        if ( $name eq "name" && $comment =~ m/^comment\d,$/ ) {
+            $class->notify_admins( %args,
+                                   reason => "badly configured spambot" );
+            return 1;
+        }
+
+        if ( ( $name eq "North Acton Station" || $name eq "Nicolas, SW6 4ST" || $name eq "Old Pack Horse, W4 5TF" || $name eq "Oriental Brasserie, W4 2HD" ) && $comment =~ m!http://[-.a-z0-9]+\.(co.cc|uni.cc|talk4fun.net|297m.com|1vn.biz|aokhost.com|freeweb7.com|myokhost.com|22web.net|totalh.com|10001mb.com|isgreat.org|66ghz.com|iblogger.org|byethost12.com|20xhost.com|yourhelpful.net|fasthoster.info|happyhost.org|by.ru|awardspace.com|awardspace.us|2kool4u.net|your-freehosting.info|150m.com|110mb.com)(\s|/)! ) {
             $class->notify_admins( %args, reason => "$1, $name" );
             return 1;
         }
@@ -98,13 +116,13 @@ sub looks_like_spam {
             return 1;
         }
 
-        if ( ( $args{added_comment} =~ /http:\/\/.*http:\/\//s )
-             || ( $args{added_comment} =~ /https:\/\/.*https:\/\//s )
-             || ( $args{added_comment} =~ /a\s+href=.*a\s+href=/s ) ) {
+        if ( ( $comment =~ /http:\/\/.*http:\/\//s )
+             || ( $comment =~ /https:\/\/.*https:\/\//s )
+             || ( $comment =~ /a\s+href=.*a\s+href=/s ) ) {
             $class->notify_admins( %args, reason => "comment with more than one URL in" );
             return 1;
         }
-        if ( $args{added_comment} =~ m{<a href=\s*></a>}is ) {
+        if ( $comment =~ m{<a href=\s*></a>}is ) {
             $class->notify_admins( %args, reason => "malformed hyperlink in comment" );
             return 1;
         }
@@ -118,8 +136,14 @@ sub looks_like_spam {
             }
         }
 
+        if ( $username =~ /^\s*spinu\s*$/is
+             && $comment =~ /^\s*respect\s*$/is ) {
+            $class->notify_admins( %args, reason => "Spinu respects us" );
+            return 1;
+        }
+
         if ( $username =~ /^[a-z]+\s[a-z]+$/ ) {
-            my $text = "$username $args{added_comment}";
+            my $text = "$username $comment";
             if ( $text =~ /q[a-tv-z].*q[a-tv-z]/ ) {
                 $class->notify_admins( %args, reason => "two-word lowercase username + two occurrences of q+non-u" );
                 return 1;

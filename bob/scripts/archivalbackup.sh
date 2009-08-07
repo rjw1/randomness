e31cac36d0ad9d671748b666f4f2d71fdfa7d75b
bob walker <bob@randomness.org.uk>
2009-08-07 21:42:03 +0000

    backup kake's a-z files.

--- bob/scripts/archivalbackup.sh
+++ bob/scripts/archivalbackup.sh
@@ -4,3 +4,9 @@ do
 /usr/bin/pg_dump -Z 9 -f /export/home/bob/archivalbackups/$db.sql.`/usr/bin/date +%Y%m%d`.gz -d $db
 done
 /opt/csw/bin/svnadmin dump -q /export/home/repository/ | bzip2 > /export/home/bob/archivalbackups/svndump.`/usr/bin/date +%Y%m%d`.bz2
+#backup up kakes a-z files
+for file in a-to-z.txt a-to-z.kml a-to-z-pubs.txt a-to-z-pubs.kml
+do
+/opt/csw/bin/wget -O /export/home/bob/archivalbackups/$file.`/usr/bin/date +%Y%m%d` -q http://the.earth.li/~kake/misc/$file
+done
+

b2936efd3e89599648039fff873bce618aa28591
bob walker <bob@randomness.org.uk>
2009-08-25 20:22:48 +0000

    thing to install modules.

---
+++ bob/scripts/installtestmodule.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+# script to add modules to my testing installs.
+
+# accept defaut modules for Module::Autoinstall modules.
+export PERL_AUTOINSTALL='--defaultdeps'
+export PERL_MM_USE_DEFAULT=1
+export AUTOMATED_TESTING=1
+export TMPDIR=$HOME/cpantesting/tmp/
+# start date
+STARTDATE=`date`
+#ensure the sun compiler is my path before anything else
+PATH=/opt/SUNWspro/bin:$PATH
+#
+# remove search.cpan stuff for input. has the side effect that just a modulename will also work. 
+# normal input should be a download link for the module.
+MODULE=`echo $1 | sed -e 's#http://search.cpan.org/CPAN/authors/id/##'`
+echo "Started $MODULE $STARTDATE"
+echo "Started $MODULE $STARTDATE" >> $HOME/cpantesting/history/tested
+# Perl versions available
+PERLVERS="5.10.0 5.10.1 5.8.9"
+
+
+#Do testing
+
+for PERL in $PERLVERS
+do
+echo $PERL
+# remove current tree if it exists
+rm -rf $HOME/cpantesting/perl-$PERL
+# untar the clean tree
+gtar -zxf $HOME/cpantesting/perl-${PERL}.tar.gz -C $HOME/cpantesting/
+# actually install the module.
+$HOME/cpantesting/perl-${PERL}/bin/cpan $MODULE
+#clean up .cpan
+rm -rf $HOME/cpantesting/perl-${PERL}/.cpan/*
+# tar up new stuff.
+gtar -zcf  $HOME/cpantesting/perl-${PERL}.tar.gz -C $HOME/cpantesting/ perl-${PERL}
+done
+
+FINISHDATE=`date`
+echo "Finished $MODULE $FINISHDATE"
+echo "Finished $MODULE $FINISHDATE" >> $HOME/cpantesting/history/tested

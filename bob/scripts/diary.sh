d1e5fcf5ed0233fbd475f11516dffa45f9066065
bob walker <bob@randomness.org.uk>
2010-05-16 12:49:23 +0000

    added diary script to generate diary html

---
+++ bob/scripts/diary.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# script to generate my diary in html 
+
+# quiety update the svn directory
+svn up -q /export/home/bob/web/vhosts/randomness.org.uk/diary/
+#generate html from diary file 
+/usr/bin/MultiMarkdown.pl /export/home/bob/web/vhosts/randomness.org.uk/diary/diary > /export/home/bob/web/vhosts/randomness.org.uk/diary/index.html

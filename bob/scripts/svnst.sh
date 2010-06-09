38ae89f7c4784af601d72b6e7c9d4e127f78da9a
bob walker <bob@randomness.org.uk>
2010-06-09 13:43:19 +0000

    made a nagios plugin

--- bob/scripts/svnst.sh
+++ bob/scripts/svnst.sh
@@ -1,23 +1,54 @@
 #!/bin/bash
-if svn st $1 2>&1 | grep "warning" > /dev/null
+# nagios plugin to check the state of a checkout.
+while getopts "d:h" option
+do
+  case $option in
+   d ) DIRTOCHECK=$OPTARG ;;
+   h ) HELP=1 ;;
+   * ) HELP=1 ;;
+  esac
+done
+if [[ -z $DIRTOCHECK ]]
 then
-echo "not a workign copy"
-elif  svn st $1 | grep "M" > /dev/null
+    echo "No directory to check"
+    HELP=1
+fi
+if [ "$HELP" = "1" ]
 then
-echo "files modified"
-elif svn st $1 | grep "?" > /dev/null 
-then 
-echo "files to add"
-elif svn st $1 | grep "A" > /dev/null 
-then 
-echo "files to add"
-elif svn st $1 | grep "!" > /dev/null 
-then 
-echo "files missing"
-elif [[svn st $1  = ""]]
+  echo "check the state of a svn checkout"
+  echo "-d directory to check"
+  echo "-h this help message"
+  exit 3
+fi
+
+
+
+if [[ `svn st $DIRTOCHECK 2>&1`  = "" ]]
 then
 echo "nothing to do"
+exit 0
+elif svn st $DIRTOCHECK 2>&1 | grep -q "warning" 
+then
+echo "not a workign copy"
+exit 3
+elif  svn st $DIRTOCHECK | grep -q "M" 
+then
+echo "files modified"
+exit 1
+elif svn st $DIRTOCHECK | grep -q "?" 
+then 
+echo "files to add"
+exit 1
+elif svn st $DIRTOCHECK | grep -q "A"
+then 
+echo "files to add"
+exit 1
+elif svn st $DIRTOCHECK | grep -q "!"
+then 
+echo "files missing"
+exit 1
 else
-beeep
+echo "not and expected result"
+exit 3
 fi
 

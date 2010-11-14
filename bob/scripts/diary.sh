9122e478333fe718922579bef94970730f6d54f9
bob walker <bob@randomness.org.uk>
2010-11-14 18:28:42 +0000

    updated to work on nimbus.

--- bob/scripts/diary.sh
+++ bob/scripts/diary.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # script to generate my diary in html 
-# to be run on nebula
+# to be run on nmbus
+export MODULEBUILDRC="/export/home/bob/perl5/.modulebuildrc"
+export PERL_MM_OPT="INSTALL_BASE=/export/home/bob/perl5"
+export PERL5LIB="/export/home/bob/perl5/lib/perl5/i386-pc-solaris2.11-thread-multi:/export/home/bob/perl5/lib/perl5"
+export PATH="/export/home/bob/perl5/bin:$PATH"
+
 
 # quiety update the svn directory
 svn up -q /export/home/bob/web/vhosts/randomness.org.uk/diary/
 #generate html from diary file 
-/usr/bin/MultiMarkdown.pl /export/home/bob/web/vhosts/randomness.org.uk/diary/diary > /export/home/bob/web/vhosts/randomness.org.uk/diary/index.html
+MultiMarkdown.pl /export/home/bob/web/vhosts/randomness.org.uk/diary/diary > /export/home/bob/web/vhosts/randomness.org.uk/diary/index.html

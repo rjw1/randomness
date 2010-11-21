4837e5af25b531e7a8143687d4c7ad5575873b9a
bob walker <bob@randomness.org.uk>
2010-11-21 17:16:21 +0000

    for loop svn backup. reap files older than 14 days make sure rsync picks up the exclude list. make it dump stuff to daily

--- bob/scripts/backup.sh
+++ bob/scripts/backup.sh
@@ -4,14 +4,20 @@
 #backup up kakes a-z files
 for file in a-to-z.txt a-to-z.kml a-to-z-pubs.txt a-to-z-pubs.kml 
 do
-wget -O /export/home/bob/backups/$file.`/usr/bin/date +%Y%m%d` -q http://the.earth.li/~kake/misc/$file
+wget -O /export/home/bob/backups/daily/$file.`/usr/bin/date +%Y%m%d` -q http://the.earth.li/~kake/misc/$file
+touch /export/home/bob/backups/daily/$file.`/usr/bin/date +%Y%m%d`
+gzip /export/home/bob/backups/daily/$file.`/usr/bin/date +%Y%m%d`
 done
 
-#backup up svn repo
-svnadmin dump -q /export/home/subversion/randomness | bzip2 > /export/home/bob/backups/svndump.randomness.`/usr/bin/date +%Y%m%d`.bz2
-svnadmin dump -q /export/home/subversion/secure | bzip2 > /export/home/bob/backups/svndump.secure.`/usr/bin/date +%Y%m%d`.bz2
+#backup up svn repos
+for repo in randomness secure bob
+do
+svnadmin dump -q /export/home/subversion/$repo | bzip2 > /export/home/bob/backups/daily/svndump.$repo.`/usr/bin/date +%Y%m%d`.bz2
+done
 
+/export/home/bob/bin/reaper.sh -a 14 -d /export/home/bob/backups/daily -f "*gz"
+/export/home/bob/bin/reaper.sh -a 14 -d /export/home/bob/backups/daily -f "*bz2"
 # backup everything to rsync.net
-rsync -az --exclude-from .rsync_exclude_list /export/home/bob/ ch-s010.rsync.net:nimbus/
+rsync -az --exclude-from /export/home/bob/.rsync_exclude_list /export/home/bob/ ch-s010.rsync.net:nimbus/
 
 

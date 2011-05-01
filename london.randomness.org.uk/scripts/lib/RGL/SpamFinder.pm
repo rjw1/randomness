bc19ea01e6d34beda0e8bf540c4373097451aa0a
bob walker <bob@randomness.org.uk>
2011-05-01 10:43:54 +0000

    Cleared out some unused heuristics, added some new ones.

--- london.randomness.org.uk/scripts/lib/RGL/SpamFinder.pm
+++ london.randomness.org.uk/scripts/lib/RGL/SpamFinder.pm
@@ -5,6 +5,9 @@ use strict;
 use Data::Dumper;
 use Email::Send;
 
+use vars qw( $VERSION );
+$VERSION = '0.01';
+
 sub looks_like_spam {
     my ( $class, %args ) = @_;
 
@@ -26,58 +29,35 @@ sub looks_like_spam {
                                    reason => "URL in category field" );
             return 1;
         }
-        if ( $cat =~ m/very nice site!/i ) {
+        if ( $cat =~ m/!/ ) {
             $class->notify_admins( %args, id => "00005",
-                                   reason => "Very Nice Site in category" );
+                                   reason => "exclamation mark in category field" );
             return 1;
         }
     }
 
+    my @locs = @{ $args{metadata}{locale} };
+    if ( ( scalar @cats == 1 ) && ( scalar @locs == 1 )
+         && $cats[0] eq $locs[0] ) {
+        $class->notify_admins( %args, id => "00006",
+                               reason => "category and locale identical" );
+        return 1;
+    }
+
     # Everything below here only matches if we come via "Add a comment".
     if ( $args{via_add_comment} ) {
-
-        if ( $comment =~ m/(http.*\.co\.cc\/sitemap)/ ){
-            $class->notify_admins( %args, id => "00014",
-                                   reason => "$1 in comment" );
-            return 1;
-        }
-
-        if ( $comment =~ m/(http.*\.tk\/sitemap)/ ){
-            $class->notify_admins( %args, id => "00015",
-                                   reason => "$1 in comment" );
-            return 1;
-        }
-
-        if ( $comment =~ m/(http.*ryjgy.edu.ms\/sitemap)/ ){
-            $class->notify_admins( %args, id => "00016",
-                                   reason => "$1 in comment" );
-            return 1;
-        }
-
-        if ( $host eq "93.126.94.14" && $name eq "Category Pool Table" ) {
-            $class->notify_admins( %args, id => "00017",
-                                   reason => "$host comment on $name" );
-            return 1;
-        }
-
-        if ( $name eq "Old Salt Quay, SE16 5QU" && $comment =~ /www\.soundclick\.com/ ) {
-            $class->notify_admins( %args, id => "00018",
-                                   reason => "soundclick comment on $name" );
-            return 1;
-        }
-
-        if ( $comment =~ m/(\+.*){7,}/ && $comment !~ m'http://' ) {
-            $class->notify_admins( %args, id => "00024",
-                                   reason => "comment with more than 7 plus "
-                                             . "signs in and no http://" );
-            return 1;
-        }
 
         if ( ( $comment =~ /http:\/\/.*http:\/\//s )
              || ( $comment =~ /https:\/\/.*https:\/\//s )
              || ( $comment =~ /a\s+href=.*a\s+href=/s ) ) {
             $class->notify_admins( %args, id => "00034",
                                reason => "comment with more than one URL in" );
+            return 1;
+        }
+
+        if ( $comment =~ m|\w{6}\s+<a\s+href="http://[a-z]{12}\.com| ) {
+            $class->notify_admins( %args, id => "00035",
+                    reason => "six-character comment plus link to 12-character URL" );
             return 1;
         }
 
@@ -103,11 +83,6 @@ sub looks_like_spam {
         if ( $content =~ 'http://' && $content =~ /\boxycodone\b/ ) {
             $class->notify_admins( %args, id => "00041",
                                    reason => "'oxycodone' + URL" );
-            return 1;
-        }
-        if ( $content =~ 'http://' && $content =~ /free\s+video\s+download/ ) {
-            $class->notify_admins( %args, id => "00042",
-                                   reason => "'free video download' + URL" );
             return 1;
         }
     }
